4c262d172a406416d854128c3bbec6605d4088cb
chahat2725 <anurag27.bhandari@simplilearn.net>
2023-04-29 15:07:02 +1000

    first commit

---
+++ class object.java
@@ -0,0 +1,9 @@
+class student {
+    int roll number;
+    str name;
+
+    public static void main(String[] args){
+        Student s1= new student;
+    //creating the student class
+    system.out.println(s1.roll number);system.out.println(s1.name);
+}}
