cec4fddd19c2642363634cd98b3194dd42f7d9ac
chahat2725 <anurag27.bhandari@simplilearn.net>
2023-04-29 15:42:06 +1000

    second commit

---
+++ class declaration.java
@@ -0,0 +1,51 @@
+public class dog {
+    string name;
+    string breed;
+    string color;
+    int age;
+
+    public static void main(String[] args){
+
+    // Declaration of class
+    public Dog("String name, string breed, string color, int age");{
+            this.name= name;
+            this.breed= breed;
+            this.color= color;
+            this.age= age;
+        }
+
+    // method 1
+    public string get
+
+    name(){ return name;}
+
+    // method2
+    public string get
+
+    breed(){return breed; }
+
+    // method 3
+    public string get
+
+    color(){return color;}
+
+    // method 4
+    public string get
+
+    age(){ return age; }
+
+    public String toString()
+        {
+            return ("Hi my name is " + this.getName()
+                    + ".\nMy breed,age and color are "
+                    + this.getBreed() + "," + this.getAge()
+                    + "," + this.getColor());
+
+    public static void main(string() args)
+
+    public static void main(String[] args) {
+        Dog tuffy = new Dog("tuffy", "papillon", 5, "white");
+        System.out.println(tuffy.toString());
+    }
+
+}}
