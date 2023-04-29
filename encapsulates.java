c694a7107a5bc1fefbb28468b94efa6f7ae22ba5
chahat2725 <anurag27.bhandari@simplilearn.net>
2023-04-29 23:56:30 +1000

    class file

---
+++ encapsulates.java
@@ -0,0 +1,33 @@
+public class calculator {
+    public static void main(String[] args)
+    int a, b;
+
+    calculator(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    int add() {
+        return a + b;
+    }
+
+    int sub() {
+        return a - b;
+    }
+
+    int mul() {
+        return a * b;
+    }
+
+    int div() {
+        return a / b;
+    }
+}
+
+public class calc {
+    public static void main(string[] args) {
+        scanner sr = new Scanner(System.in);
+        system.out.print("Enter a : ");
+        system.out.print("Enter b : ");
+    }
+}
