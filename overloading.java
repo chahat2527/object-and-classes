61a95a8b5f98da54135cb60747d317171da1c295
chahat2725 <anurag27.bhandari@simplilearn.net>
2023-05-01 16:04:28 +1000

    class file

---
+++ overloading.java
@@ -0,0 +1,19 @@
+public class test {
+    public static void main(String[] args)
+
+    add numbers(10, 20,30);
+}Static
+
+    void add
+
+    numbers (int a, int b){
+    system.out.println("sum is" + (a+b));
+}
+
+{
+    static void add
+
+    numbers(int a, int b, int c){
+        system.ou.println("sum is" + (a+b+c));
+    }
+}
