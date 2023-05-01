892167d1dc75d111275a87a003ba39f4d727d7d9
chahat2725 <anurag27.bhandari@simplilearn.net>
2023-05-01 15:42:47 +1000

    class file

---
+++ data hiding.java
@@ -0,0 +1,18 @@
+public class movie {
+    private string p1 = "squid game";
+
+    public string getp1() {
+        return p1;
+    }
+
+    public void set
+
+    p1(string p1){
+        this.p1=p1;
+    }
+}
+
+public class datahiding {
+    public static void main(String[] args)
+    Movie m=(m.getp1());
+}
